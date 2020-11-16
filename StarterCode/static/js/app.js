935e7b030ec7a5a1c9dfa93d9888455add6ffd05
Erika Yi <68488125+akire2iy@users.noreply.github.com>
2020-11-16 15:00:41 -0500

    4th commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -11,6 +11,12 @@ function buildPlot(BellyButtonData) {
     var bbtype = data.metadata.map(bb => bb.bbtype);
     console.log(`Belly Button Types: ${bbtype}`);
 
+    var washingfreq = data.metadata.map(wf => wf.wfreq);
+    console.log(`Washing Frequency: ${washingfreq}`);
+
+    var place = data.metadata.map(plc => plc.location);
+    console.log(`location each sample: ${place}`);
+
     // Filter by ids
     var sample_values = data.samples.filter(sampleid => sampleid.id);
     console.log(sample_values);
@@ -76,6 +82,32 @@ function buildPlot(BellyButtonData) {
       Plotly.newPlot('bubble', data, layout);
 
       // create gauge chart for washing frequency of the individual 
+
+      // var data = [
+      //   {
+      //     domain: { x: [0, 1], y: [0, 1] },
+      //     value: 450,
+      //     title: { text: "Speed" },
+      //     type: "indicator",
+      //     mode: "gauge+number+delta",
+      //     delta: { reference: 380 },
+      //     gauge: {
+      //       axis: { range: [null, 500] },
+      //       steps: [
+      //         { range: [0, 250], color: "lightgray" },
+      //         { range: [250, 400], color: "gray" }
+      //       ],
+      //       threshold: {
+      //         line: { color: "red", width: 4 },
+      //         thickness: 0.75,
+      //         value: 490
+      //       }
+      //     }
+      //   }
+      // ];
+      
+      // var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+      // Plotly.newPlot('myDiv', data, layout);
 
 });
 
