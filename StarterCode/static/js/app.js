095dcdf86f79c223b2adc1b2ed3a8f3c5cda0475
Erika Yi <68488125+akire2iy@users.noreply.github.com>
2020-11-16 19:56:25 -0500

    5th commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,8 +1,8 @@
 
 function buildPlot(BellyButtonData) {
 
-    // Fetch the JSON data and console log it
-    d3.json("../../samples.json").then(function(data) {
+  // Fetch the JSON data and console log it
+  d3.json("../../samples.json").then(function (data) {
     console.log(data);
 
     var ethnicity = data.metadata.map(race => race.ethnicity);
@@ -12,18 +12,20 @@ function buildPlot(BellyButtonData) {
     console.log(`Belly Button Types: ${bbtype}`);
 
     var washingfreq = data.metadata.map(wf => wf.wfreq);
-    console.log(`Washing Frequency: ${washingfreq}`);
+    console.log(washingfreq);
 
-    var label = data.metadata.map(race => race.ethnicity);
+    var race = data.metadata.map(race => race.ethnicity);
+    console.log(race);
 
-    // Filter by ids
-    var sample_values = data.samples.filter(sampleid => sampleid.id);
-    console.log(sample_values);
+    // Filter by ids and convert object to string using toString()
+    // Take a look at the documentation about toString: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+    var sample = data.samples.filter(sampleid => sampleid.id.toString() === BellyButtonData);
+    console.log(sample);
 
     // Select one of the samples from the array
-    var oneSample = sample_values[6];
+    var oneSample = sample[0];
     console.log(oneSample);
-  
+
     // get top 10 sample_values of an individual
     var top10values = oneSample.sample_values.slice(0, 10); // includes 0 and excluding 10 to get only top 10
     console.log(top10values);
@@ -48,94 +50,156 @@ function buildPlot(BellyButtonData) {
 
     // put in the bar chart
     var OTU_barChart = [{
-        type: 'bar',
-        x: top10values,
-        y: otu_label,
-        text: labels,
-        orientation: 'h'
-      }];
-      
-      Plotly.newPlot('bar', OTU_barChart);
+      type: 'bar',
+      x: top10values,
+      y: otu_label,
+      text: labels,
+      orientation: 'h'
+    }];
+
+    Plotly.newPlot('bar', OTU_barChart);
 
     // make a bubble chart of each sample
     var trace1 = {
-        x: oneSample.otu_ids,
-        y: oneSample.sample_values,
-        text: oneSample.otu_labels,
-        mode: 'markers',
-        marker: {
-          color: oneSample.otu_ids,
-          size: oneSample.sample_values,
-          sizemode: 'area'
-        }
-      };
-      
-      var data = [trace1];
-      
-      var layout = {
-        title: 'Sample Values vs. Each OTU IDs',
-        xaxis: {title: "OTU ID"},
-        showlegend: false
-      };
-      
-      Plotly.newPlot('bubble', data, layout);
+      x: oneSample.otu_ids,
+      y: oneSample.sample_values,
+      text: oneSample.otu_labels,
+      mode: 'markers',
+      marker: {
+        color: oneSample.otu_ids,
+        size: oneSample.sample_values,
+        sizemode: 'area'
+      }
+    };
 
-      // create gauge chart for washing frequency of the individual 
+    var data = [trace1];
 
-      var washingdata = [
-        { domain: { x: [0, 1], y: [0, 1] },
-          value: parseFloat(washingfreq),
-          title: { text: "Belly Button Washing Frequency"},
-          type: "indicator",
-          mode: "gauge+number",
-          // delta: { reference: parseFloat(washingfreq) },
-          hoverinfo: "labels",
-          gauge: {
-            axis: { range: [null, 9] },
-            steps: [
-              { range: [0, 1], color: "beige" },
-              { range: [1, 2], color: "azure" },
-              { range: [2, 3], color: "lightcyan" },
-              { range: [3, 4], color: "powderblue" },
-              { range: [4, 5], color: "lightblue" },
-              { range: [5, 6], color: "lightskyblue" },
-              { range: [6, 7], color: "lightsteelblue" },
-              { range: [7, 8], color: "thistle"},
-              { range: [8, 9], color: "plum"}
-            ],
-            threshold: {
-              line: { color: "red", width: 5 },
-              thickness: 1,
-              value: washingfreq
-            }
+    var layout = {
+      title: 'Sample Values vs. Each OTU IDs',
+      xaxis: { title: "OTU ID" },
+      showlegend: false
+    };
+
+    Plotly.newPlot('bubble', data, layout);
+
+    // create gauge chart for washing frequency of the individual 
+
+    var washingdata = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        // Note: documentation on parseFloat: https://www.w3schools.com/jsref/jsref_parsefloat.asp
+        value: parseFloat(washingfreq),
+        title: { text: "Belly Button Washing Frequency" },
+        type: "indicator",
+        mode: "gauge+number",
+        // delta: { reference: parseFloat(washingfreq) },
+        hoverinfo: "labels",
+        gauge: {
+          axis: { range: [null, 9] },
+          steps: [
+            { range: [0, 1], color: "beige" },
+            { range: [1, 2], color: "azure" },
+            { range: [2, 3], color: "lightcyan" },
+            { range: [3, 4], color: "powderblue" },
+            { range: [4, 5], color: "lightblue" },
+            { range: [5, 6], color: "lightskyblue" },
+            { range: [6, 7], color: "lightsteelblue" },
+            { range: [7, 8], color: "thistle" },
+            { range: [8, 9], color: "plum" }
+          ],
+          threshold: {
+            line: { color: "green", width: 5 },
+            thickness: 1,
+            value: parseFloat(washingfreq)
           }
         }
-        ];
-      
-      var layout = { 
-        width: 600, 
-        height: 450, 
-        margin: { t: 0, b: 0 },
-        "annotations": [
-          {
-            "text": "Scrubs per Week"
-          },
-        ]
-      };
+      }
+    ]
 
-      Plotly.newPlot('gauge', washingdata, layout);
+    var layout = {
+      width: 600,
+      height: 450,
+      margin: { t: 0, b: 0, r: 0, l: 0},
+      "annotations": [
+        {
+          "text": "Scrubs per Week"
+        },
+      ]
+    }
 
-});
+    Plotly.newPlot('gauge', washingdata, layout);
 
-};
+  })
 
-
-// Display the sample metadata, for example, an individual's demographic info
-
-
+}
 
 // Display each key-value pair from the metadata JSON object on the page
-
 // Update all of the plots any time that new sample is selected
 
-buildPlot();
+function updatePlotly(BellyButtonData) {
+
+  // Fetch the JSON data and console log it
+  d3.json("../../samples.json").then((data) => {
+
+    console.log(data);
+
+    // Display the sample metadata, for example, an individual's demographic info
+    var metadata = data.metadata;
+    console.log(metadata);
+
+    // Filter by ids and convert object to string 
+    // Take a look at the documentation about toString: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+    var demo_id = metadata.filter(meta => meta.id.toString() === BellyButtonData);
+    console.log(demo_id);
+
+    var demoIDvalue = demo_id[0];
+    console.log(demoIDvalue);
+
+    // Use D3 to select the demographic info
+    var demoINFO = d3.select("#sample-metadata");
+
+    // empty out demographic info each time selecting new data 
+    demoINFO.html("");
+
+    // grab demographic info by id and put values in the demographic table   
+    Object.entries(demoIDvalue).forEach((value) => {
+      demoINFO.append("h6")
+              .text(value[0].toUpperCase() + ": " + value[1]);
+    });
+    
+  });
+
+}
+
+// update all for the plots when a new sample is selected
+function optionChanged(BellyButtonData){
+  buildPlot(BellyButtonData);
+  updatePlotly(BellyButtonData);
+};
+
+// Call updatePlotly() when a change takes place to the DOM
+d3.selectAll("#selDataset").on("change", buildPlot, updatePlotly);
+
+// This function is called when a dropdown menu item is selected
+function init() {
+
+  // read json data 
+  d3.json("../../samples.json").then((data) => {
+    console.log(data);
+
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.select("#selDataset");
+
+  data.names.forEach(function(idName) {
+    dropdownMenu.append("option")
+                .text(idName)
+                .property("value");
+  })
+
+  buildPlot(data.names[0]);
+  updatePlotly(data.names[0]);
+
+  })
+}
+
+init();
